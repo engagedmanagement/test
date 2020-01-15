df193ab61d9566410172bd0e8868a8097eb3e4b2
engagedmanagement <orgmanagement48@gmail.com>
2020-01-14 18:54:50 -0800

    Second commit

--- test.R
+++ test.R
@@ -1,1 +1,14 @@
-a=1
+library(tidyverse)
+tidyverse
+install.packages("tidyverse")
+install.packages("tidyverse", dependencies = TRUE)
+data("mpg")
+cleanup<- theme(panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank(),
+                axis.line = element_line(color = "black"))
+View(mpg)
+trans<- ggplot(mpg, aes(trans))
+trans+
+  geom_bar()+
+  cleanup
